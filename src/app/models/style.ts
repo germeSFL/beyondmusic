56e37412b2c6689efb05b57e59b1a2cd4beca878
germeSFL <63351251+germeSFL@users.noreply.github.com>
2022-12-22 10:43:48 +0100

    color palette, styling

---
+++ src/app/models/style.ts
@@ -0,0 +1,13 @@
+enum Colors {
+  color_1 = '#a6a2c3',
+  color_2 = '#d2d1e0',
+  color_3 = '#e0e0eb',
+  color_4 = '#9fa6c6',
+  color_5 = '#a8b3d7',
+  color_6 = '707488',
+  color_7 = '403e4b',
+}
+
+export class Style {
+  public Colors = Colors;
+}
